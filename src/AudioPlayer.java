33f091c62e083c939ed54da1a3ba119d7cc89aaa
Yash-Bhandari <33205775+Yash-Bhandari@users.noreply.github.com>
2019-05-01 15:24:06 -0600

    Created AudioPlayer class)

---
+++ src/AudioPlayer.java
@@ -0,0 +1,45 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class AudioPlayer {
+    Clip clip;
+    FloatControl volume;
+    public AudioPlayer(String audioFileLocation) {
+        try {
+            AudioInputStream stream = AudioSystem.getAudioInputStream(new File(audioFileLocation));
+            clip = AudioSystem.getClip();
+            clip.open(stream);
+            clip.start();
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            volume = (FloatControl)clip.getControl(FloatControl.Type.MASTER_GAIN);
+            volume.setValue(-20);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void play() {
+        clip.start();
+    }
+
+    public void pause() {
+        clip.stop();
+    }
+
+    public float getVolume() {
+        return volume.getValue();
+    }
+
+    public void setVolume(int newVolume) {
+        volume.setValue(newVolume);
+    }
+
+    public void decreaseVolume() {
+        volume.setValue(volume.getValue() - (float)0.1);
+    }
+
+    public void increaseVolume() {
+        volume.setValue(volume.getValue() + (float)0.1);
+    }
+}
--- src/Timer.java
+++ src/Timer.java
@@ -2,8 +2,29 @@ public class Timer {
     long startTime;
     long endTime;
 
-    public Timer(int durationMS) {
+    public Timer(int mins, int seconds) {
         startTime = System.currentTimeMillis();
-        endTime = startTime + durationMS;
+        endTime = startTime + seconds * 1000 + mins * 60 * 1000;
+    }
+
+    /**
+     * @return seconds since timer created
+     */
+    public int getTime() {
+        return (int)(System.currentTimeMillis() - startTime) / 1000;
+    }
+
+    /**
+     * @return seconds until timer end
+     */
+    public int getTimeLeft() {
+        return (int)(endTime - System.currentTimeMillis()) / 1000;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Timer t = new Timer(0, 10);
+        System.out.println(t.getTime());
+        Thread.sleep(5000);
+        System.out.println(t.getTime());
     }
 }
